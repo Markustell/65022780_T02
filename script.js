7c8d4c24f360006741f0c249bc4aaa653667c6ac
t31ly <woraponklabsri7731@gmail.com>
2023-01-18 20:35:12 +0700

    [all files] update

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-let text = document.getElementById("text-output");
+let text = document.getElementById("textOutput");
 
 // [input]Focus and Blur
 let FocusBx = (NameTxt) => NameTxt.style.background = "yellow";
 let BlurBx = (NameTxt) => NameTxt.style.background = "#fff";
 
-let txtOut = (val) => {
-    
+let txtOut = () => {
+    let Fn = document.forms["FORM"]["fname"].value;
+    let Sn = document.forms["FORM"]["sname"].value;
+    let Cld = document.getElementById("calendar");
+    let Lan = document.getElementById("lang");
 }
