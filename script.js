038f33c398516cf49b7bf2ab01cce8a0fb3bc4ed
t31ly <woraponklabsri7731@gmail.com>
2023-01-18 22:01:07 +0700

    [all files, add png]update

--- script.js
+++ script.js
@@ -7,17 +7,18 @@ let BlurBx = (NameTxt) => NameTxt.style.background = "#fff";
 let txtOut = () => {
     let Fn = document.forms["FORM"]["fname"].value;
     let Sn = document.forms["FORM"]["sname"].value;
-    let Cld = document.getElementById("calendar");
-    let Lan = document.getElementById("lang");
+    let Cld = document.getElementById("calendar").value;
+    let Lan = document.getElementById("lang").value;
 
-    if(Fn != null || Fn != "", Sn != null || Sn != "", Cld.value, Lan.value != "unselect") {
+    if(Fn != null || Fn != "", Sn != null || Sn != "", Cld) {
+        let showLan = Lan != "unselect" ? Lan : "";
         text.style.color = "green";
-        text.innerHTML = Fn + "," + Sn + "," + Cld.value + "," + Lan.value;
+        text.innerHTML = Fn + "," + Sn + "," + Cld + "," + showLan;
     } else {
         let alertFn = Fn == null || Fn == "" ? "Insert Name<br>" : "";
         let alertSn = Sn == null || Sn == "" ? "Insert Surname<br>" : "";
-        let alertCld = !Cld.value ? "Insert Birth date<br>" : "";
-        let alertLan = Lan.value == "unselect" ? "Select Language" : "";
+        let alertCld = Cld == "" ? "Insert Birth date<br>" : "";
+        let alertLan = Lan == "unselect" ? "Select Language" : "";
         text.style.color = "red";
         text.innerHTML = alertFn + alertSn + alertCld + alertLan;
     }
