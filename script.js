584a9c1f2d1194e76155667498ca2f3299d92f53
t31ly <woraponklabsri7731@gmail.com>
2023-01-18 22:09:16 +0700

    [script]add comment

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// set
 let text = document.getElementById("textOutput");
 
 // [input]Focus and Blur
 let FocusBx = (NameTxt) => NameTxt.style.background = "yellow";
 let BlurBx = (NameTxt) => NameTxt.style.background = "#fff";
 
+// [p]Output
 let txtOut = () => {
     let Fn = document.forms["FORM"]["fname"].value;
     let Sn = document.forms["FORM"]["sname"].value;
