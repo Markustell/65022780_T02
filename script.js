a4bc1031534ff2ab3b4ce287e51a56589d5001c3
t31ly <woraponklabsri7731@gmail.com>
2023-01-18 19:24:48 +0700

    [index.html, script.js]update

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+let text = document.getElementById("text-output");
+
+// [input]Focus and Blur
 let FocusBx = (NameTxt) => NameTxt.style.background = "yellow";
 let BlurBx = (NameTxt) => NameTxt.style.background = "#fff";
+
+let txtOut = (val) => {
+    
+}
